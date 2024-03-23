edac173730a242e36144a50942a01eae4258cabb
MerBudd <82082386+MerBudd@users.noreply.github.com>
2024-03-23 21:20:10 +0300

    Update Techiee.py

--- Techiee.py
+++ Techiee.py
@@ -9,6 +9,9 @@ from discord import app_commands
 from typing import Optional, Dict
 import shelve
 
+from keep_alive import keep_alive
+keep_alive()
+
 #---------------------------------------------AI Configuration-------------------------------------------------
 genai.configure(api_key=GOOGLE_AI_KEY)
 
