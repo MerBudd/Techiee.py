2baca147e211b4b2337bef591d157eb1a215c18a
MerBudd <82082386+MerBudd@users.noreply.github.com>
2025-04-24 16:44:08 +0300

    Update Techiee.py

--- Techiee.py
+++ Techiee.py
@@ -128,30 +128,7 @@ async def generate_response_with_text(message_text):
         response = client.models.generate_content(
             model=gemini_model,
             contents=message_text,
-            config=types.GenerateContentConfig(
-			safety_settings= [
-		types.SafetySetting(
-        		category='HARM_CATEGORY_HARASSMENT',
-        		threshold='BLOCK_NONE'
-    		),
-    		types.SafetySetting(
-        		category='HARM_CATEGORY_HATE_SPEECH',
-        		threshold='BLOCK_NONE'
-    		),
-    		types.SafetySetting(
-        		category='HARM_CATEGORY_SEXUALLY_EXPLICIT',
-        		threshold='BLOCK_NONE'
-    		),
-    		types.SafetySetting(
-        		category='HARM_CATEGORY_DANGEROUS_CONTENT',
-        		threshold='BLOCK_NONE'
-    		),
-    		types.SafetySetting(
-        		category='HARM_CATEGORY_CIVIC_INTEGRITY',
-        		threshold='BLOCK_NONE'
-    		),
-			] 
-	),
+            config=generation_configs
     )
 
         if response.has_error:  # Check for errors in a cleaner way
